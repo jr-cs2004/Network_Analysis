e77a30349878baa19524c0270e776f3992dd63f7
Javad <jr.cs2004@gmail.com>
2019-11-13 18:29:09 +0330

    Drawing the graph is added.

--- main.py
+++ main.py
@@ -5,6 +5,21 @@ import operator
 file_name = "test.txt"
 G = nx.read_edgelist(file_name, nodetype=str, data=(('weight', int),)) #reading a graph as edge listed
 # G.degree()  ----  List of nodes and their degrees
+degree_sequence = sorted(G.degree().items(), key=operator.itemgetter(1), reverse = True)
+
+## draw graph in inset
+plt.title("Degree rank plot")
+plt.ylabel("degree")
+plt.xlabel("rank")
+plt.axes([0.45, 0.45, 0.45, 0.45])
+Gcc = G.subgraph(sorted(nx.connected_components(G), key=len, reverse=True)[0])
+pos = nx.spring_layout(Gcc)
+plt.axis('off')
+nx.draw_networkx_nodes(Gcc, pos, node_size=20)
+nx.draw_networkx_edges(Gcc, pos, alpha=0.4)
+plt.show()
+
+#sort nodes based on their degrees and removed the top one
 i = 0
 print (G.number_of_nodes())
 while i < (G.number_of_nodes() / 10):
@@ -13,3 +28,4 @@ while i < (G.number_of_nodes() / 10):
   G.remove_node(degree_sequence[0][0])
   i = i + 1
 print (G.number_of_nodes())
+
