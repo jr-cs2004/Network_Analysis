fc8eef31bb34acb38909598fda819317a2434489
rezaei <jr.cs2004@gmail.com>
2021-02-08 12:43:27 +0330

    DEG Processed

--- Drosophila Melanogaster/DEG/Process_DEG_Data.py
+++ Drosophila Melanogaster/DEG/Process_DEG_Data.py
@@ -50,50 +50,100 @@ print('# of unique genes: ' + str(len(gene_names)) + '\n')
 # ######################################################################################
 # ######################################################################################
    
-UniProt_file = open("DROME_7227_idmapping.dat", "r")
-
-uniProt_ids = []
-other_id_types = []
-other_id_values = []
+UniProt_file = open("Uniprot_ID_Mapping_for_Drosophila_Complete_File.txt", "r")
+uniProt_IDs = {}
 for line in UniProt_file:
-    uniProt_ids.append(line.split()[0])
-    other_id_types.append(line.split()[1])
-    other_id_values.append(line.split()[2])
-
-print('# of uniProt_ids: ' + str(len(uniProt_ids)) + '\n')
-
+    uniProt_IDs[line.split()[0]] = {}
+UniProt_file.close()
+print('# of uniProt_IDs: ' + str(len(uniProt_IDs.keys())) + '\n')
+   
+UniProt_file = open("Uniprot_ID_Mapping_for_Drosophila_Complete_File.txt", "r")
+for line in UniProt_file:
+    uniProt_IDs[line.split()[0]][line.split()[1]] = line.split()[2]
+UniProt_file.close()
 
 
 # ######################################################################################
 # ######################################################################################
 # ######################################################################################
 
+gene_names_to_uniProt_IDs_and_FlyBase_IDs = [[], [], []]
 
-n = len(uniProt_ids)
-_uniProt_ids = []
-_other_id_types = []
-_other_id_values = []
+all_gene_names_in_UniProt = []
+
+counter = 0
+for uniProt_ID, value in uniProt_IDs.items():
+   gene_name = ''
+   FlyBase_ID = ''
+   for x, y in value.items():
+      if (x == 'Gene_Name'):
+         gene_name = y
+      if (x == 'FlyBase'):
+         FlyBase_ID = y
+   if (gene_name != '' and FlyBase_ID != '' and gene_name in gene_names):
+      gene_names_to_uniProt_IDs_and_FlyBase_IDs[0].append(uniProt_ID)
+      gene_names_to_uniProt_IDs_and_FlyBase_IDs[1].append(gene_name)
+      gene_names_to_uniProt_IDs_and_FlyBase_IDs[2].append(FlyBase_ID)
+      all_gene_names_in_UniProt.append(gene_name)
+   if (gene_name != '' and FlyBase_ID == ''):
+      counter += 1
+
+print('# of unique gene names in UniProt: ' + str(len(list(set(all_gene_names_in_UniProt)))) + '\n')
+print('counter: ', counter)
+
+_file = open("gene_names_UniProt_IDs_FlyBase_IDs.txt", "w")
+n = len(gene_names_to_uniProt_IDs_and_FlyBase_IDs[0])
+gene_name_to_FlyBase_IDs_hash = {}
+gene_name_to_UniProt_IDs_hash = {}
 for i in range(0, n):
-    if (other_id_types[i] == 'Gene_Name'):
-        _uniProt_ids.append(uniProt_ids[i])
-        _other_id_types.append(other_id_types[i])
-        _other_id_values.append(other_id_values[i])
-print('# of _uniProt_ids: ' + str(len(_uniProt_ids)) + '\n')
-gene_names_to_UniProt_IDs = [[], []]
-m = len(gene_names)
-n = len(_uniProt_ids)
-for i in range(0, m):
-    counter = 0
-    for j in range(0 , n):
-        if (_other_id_values[j] == gene_names[i]):
-            counter += 1
-            gene_names_to_UniProt_IDs[0].append(gene_names[i])
-            gene_names_to_UniProt_IDs[1].append(_uniProt_ids[j])
+   _file.write(gene_names_to_uniProt_IDs_and_FlyBase_IDs[0][i] + '\t' + gene_names_to_uniProt_IDs_and_FlyBase_IDs[1][i] + '\t' + gene_names_to_uniProt_IDs_and_FlyBase_IDs[2][i] + '\n')
+   gene_name_to_FlyBase_IDs_hash[gene_names_to_uniProt_IDs_and_FlyBase_IDs[1][i]] = []
+   gene_name_to_UniProt_IDs_hash[gene_names_to_uniProt_IDs_and_FlyBase_IDs[1][i]] = []
 
-test_file = open("test.txt", "w")
-n = len(gene_names_to_UniProt_IDs[0])
 for i in range(0, n):
-    test_file.write(gene_names_to_UniProt_IDs[0][i] + '\t' + gene_names_to_UniProt_IDs[1][i] + '\n')
+   gene_name_to_FlyBase_IDs_hash[gene_names_to_uniProt_IDs_and_FlyBase_IDs[1][i]].append(gene_names_to_uniProt_IDs_and_FlyBase_IDs[2][i])
+   gene_name_to_UniProt_IDs_hash[gene_names_to_uniProt_IDs_and_FlyBase_IDs[1][i]].append(gene_names_to_uniProt_IDs_and_FlyBase_IDs[0][i])
+
+_file = open("gene_names_to_FlyBase_IDs.txt", "w")
+_file_2 = open("gene_names_to_FlyBase_IDs_non_unique.txt", "w")
+for x in gene_name_to_FlyBase_IDs_hash:
+   if (len(list(set(gene_name_to_FlyBase_IDs_hash[x]))) == 1):
+      _file.write(x + '\t' + gene_name_to_FlyBase_IDs_hash[x][0] + '\n')
+   else:
+      _file_2.write(x + '\t')
+      _temp_list = list(set(gene_name_to_FlyBase_IDs_hash[x]))
+      for y in _temp_list:
+         _file_2.write(y + '\t')
+      _file_2.write('\n')
+
+_file = open("gene_names_to_UniProt_IDs.txt", "w")
+for x, y in gene_name_to_UniProt_IDs_hash.items():
+   _file.write(x + '\t')
+   for z in y:
+      _file.write(z + '\t')
+   _file.write('\n')
 
 
-print('# of unique finds: ' + str(len(list(set(gene_names_to_UniProt_IDs[0])))) + '\n')
+
+# this file is downloaded directly from flybase.org as FlyBase IDs of 
+# essential genes reported by DEG
+_file = open("FlyBase_IDs_Extracted_Using_flybase.org_website.txt", "r")
+gene_ids_1 = []
+for line in _file:
+   gene_ids_1.append(line.split()[0])
+
+
+# this file contains FlyBase IDs of essential genes which we extracted
+# by processing DEG gene_names and UniProt ID mapping
+_file = open("gene_names_to_FlyBase_IDs.txt", "r")
+gene_ids_2 = []
+for line in _file:
+   gene_ids_2.append(line.split()[1])
+
+Deg_Essential_FlyBase_IDs_intersection_between_flybase_org_N_handy_exracted = list(set(gene_ids_1) & set(gene_ids_2))
+# it seems the progress is in true direction! see the result.
+print(len(Deg_Essential_FlyBase_IDs_intersection_between_flybase_org_N_handy_exracted))
+_file = open("gene_names_to_FlyBase_IDs_intersection_between_flybase.org_N_handy_exracted.txt", "w")
+for x in Deg_Essential_FlyBase_IDs_intersection_between_flybase_org_N_handy_exracted:
+   _file.write(x + '\n')
+
